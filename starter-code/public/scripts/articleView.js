608aec59083ce64c52f424e9343a5dbf0e63b5f5
Brianna <briannaburrows@gmail.com>
2017-03-28 11:21:46 -0700

    completed all TODOS

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -113,6 +113,7 @@
     // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
     // appending to the DOM.
     // The callback is not required to return anything.
+    Article.numWordsAll();
     Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
